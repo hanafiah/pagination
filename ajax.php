cd1521769ba3fea3ea177f8ea3452a1424112d33
hanafiah <ibnuyahya@gmail.com>
2013-11-15 12:16:07 +0800

    initial file

---
+++ ajax.php
@@ -0,0 +1,35 @@
+<?php
+
+$desc = array(
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit scelerisque enim nec rutrum. Sed semper velit erat, in varius purus pretium ut. Cras sollicitudin, sem nec faucibus venenatis, sapien augue facilisis urna, dictum ornare massa diam eu lorem. Nam id dui facilisis, semper lectus a, commodo augue. Fusce et porttitor eros. Fusce aliquam venenatis dolor, at tincidunt tellus. Maecenas suscipit erat orci, vel bibendum leo dictum dapibus. Sed fermentum volutpat velit, vitae tempor elit tempor pharetra.',
+    'Curabitur molestie sollicitudin ligula, ac laoreet velit ornare vitae. Donec semper sollicitudin dui ut mollis. Aenean blandit tellus ut lacus porttitor, eu accumsan arcu auctor. Proin nec nibh blandit, euismod lacus in, sollicitudin lectus. Nam auctor felis elementum sapien tincidunt imperdiet. Nulla sed elit sollicitudin, porta tellus vel, eleifend massa. Mauris eu lacus libero. Donec suscipit libero augue, sed laoreet arcu molestie id. In consectetur purus ut ante egestas, at mattis sapien vulputate. Quisque non convallis lectus, ac volutpat lacus. Nulla consectetur libero ac erat volutpat, ultricies ullamcorper metus vehicula. Cras a gravida orci.',
+    'Suspendisse arcu mi, mattis vitae gravida et, luctus consectetur ipsum. Vivamus luctus, diam ac blandit porttitor, erat justo tempus ligula, id consequat leo mi ac lectus. Pellentesque aliquet ipsum vel felis varius vulputate. Etiam aliquet arcu a magna scelerisque, quis interdum nunc semper. Morbi aliquet pretium urna sed ornare. Sed velit risus, euismod egestas tincidunt a, dictum at tortor. Aenean euismod quam et eros posuere tempor. Phasellus eleifend elementum eros vitae ultrices. Quisque in sem ut neque vulputate viverra. In id leo imperdiet, varius ligula a, mattis massa. Suspendisse in consequat turpis.',
+    'Duis tincidunt mollis vulputate. In adipiscing sed odio nec ultrices. Donec ac lacinia risus, sit amet semper tortor. Ut ultricies varius nulla, eu lacinia purus elementum nec. Curabitur sit amet leo ac arcu aliquam placerat. Curabitur viverra dictum ipsum ac molestie. Phasellus non neque dui. Aenean at eros vel orci sollicitudin rhoncus vel sit amet leo. Suspendisse sed arcu sed nunc pulvinar dapibus sit amet vel eros. Nam feugiat felis quis lacinia rhoncus.',
+    'Aliquam ultricies urna vel fermentum molestie. Suspendisse ut condimentum erat. Proin tempus laoreet tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus sodales augue nulla, non ullamcorper felis sagittis ac. Nullam imperdiet ultricies neque, ut viverra ligula bibendum et. Nullam laoreet a justo ut lobortis. In rhoncus mollis elementum. Mauris gravida augue vitae nulla auctor, quis eleifend nisl adipiscing. Cras vulputate lobortis porttitor. Phasellus vehicula metus ut mauris facilisis bibendum. Aliquam et diam elementum metus gravida blandit ac sagittis velit. Donec non pellentesque sapien, nec vulputate mauris.'
+);
+$table = array();
+for ($x = 1; $x <= 500; $x++) {
+    $table[] = array(
+        'id' => $x,
+        'name' => 'my title ' . $x,
+        'url' => 'http://example.com?x=' . $x,
+        'desc' => $desc[$x % 4],
+    );
+}
+
+$start = $_GET['offset'];
+$limit = $_GET['limit'];
+
+//cache data
+//we only return some range of data
+$data['data'] = array_slice($table, $start - 1, $limit);
+
+$data['total'] = count($table);
+$data['start'] = (int) $start;
+$data['limit'] = (int) $limit;
+
+header("Expires: 0");
+header("Cache-Control: no-cache, must-revalidate, post-check=0, pre-check=0");
+header("Pragma: no-cache");
+header('Content-type: application/json');
+echo json_encode($data);
---
+++ jquery.pagination.js
@@ -0,0 +1,157 @@
+/**
+ File:        jquery.pagination.js
+ Version:     1.0.0
+ Author:      Muhamad Hanafiah Yahya @ ibnuyahya ( hanafiahyahya.blogspot.com )
+ source:      https://github.com/hanafiah/pagination
+
+
+ Copyright (c) 2013 ibnuyahya
+ http://opensource.org/licenses/MIT
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+(function($) {
+    $.fn.pagination = function(options) {
+        var base = this;
+        var cache = {
+            json: {}
+        };
+        var data = {};
+        var postData = '';
+        var defaults = {
+            ajaxSource: '',
+            iCurrentPage: 1,
+            pageLength: 5,
+            itemsOnPage: 10,
+            callback: function() {
+            }
+        };
+        var settings = $.extend({}, defaults, options);
+        cache.offset = settings.iCurrentPage;
+        cache.limit = settings.itemsOnPage * (settings.pageLength * 3);
+
+
+        var fnGetData = function() {
+            postData = {
+                offset: cache.offset,
+                limit: cache.limit
+            };
+            $.ajaxSetup({
+                async: false
+            });
+            $.getJSON(settings.ajaxSource, postData, function(d) {
+                cache.json = jQuery.extend(true, {}, d);
+                cache.iLower = cache.json.start - 1;
+                cache.iUpper = cache.json.start + cache.json.limit - 1;
+            });
+        };
+
+        var fnDraw = function(iPage) {
+            settings.iCurrentPage = parseInt(iPage);
+            fnRenderPaging();
+            fnSentData();
+        };
+
+        var fnSentData = function() {
+            iOffset = (settings.iCurrentPage - 1) * settings.itemsOnPage;
+            iLimit = settings.itemsOnPage;
+
+            //check if require pull data
+            if (cache.iLower < 0 || iOffset < cache.iLower || (iOffset + iLimit) > cache.iUpper)
+            {
+                cache.offset = iOffset - Math.floor(settings.pageLength * settings.itemsOnPage);
+                if (cache.offset < 1) {
+                    cache.offset = 1;
+                }
+                fnGetData();
+            }
+            data = cache.json.data.slice(iOffset - cache.iLower, iLimit + iOffset - cache.iLower);
+            if (typeof settings.callback == 'function') { // make sure the callback is a function
+                settings.callback(data); // brings the scope to the callback
+            }
+        };
+
+        var fnRenderPaging = function() {
+            settings.iTotalPages = Math.ceil(cache.json.total / settings.itemsOnPage);
+            var iHalf = Math.floor(settings.pageLength / 2);
+            if (settings.iTotalPages < settings.pageLength) {
+                iStart = 1;
+                iEnd = settings.iTotalPages;
+            } else if (settings.iCurrentPage <= iHalf) {
+                iStart = 1;
+                iEnd = settings.pageLength;
+            } else if (settings.iCurrentPage >= (settings.iTotalPages - iHalf)) {
+                iStart = settings.iTotalPages - settings.pageLength + 1;
+                iEnd = settings.iTotalPages;
+            } else {
+                iStart = settings.iCurrentPage - iHalf;
+                iEnd = iStart + settings.pageLength - 1;
+            }
+
+            $('li:gt(0)', base).filter(':not(.next,.last)').remove();
+            for (j = iStart; j <= iEnd; j++) {
+                sClass = (j == settings.iCurrentPage) ? 'class="active"' : '';
+                $('<li ' + sClass + '><a href="#">' + j + '</a></li>')
+                        .insertBefore($('.next,.last', base)[0])
+                        .bind('click', function(e) {
+                            e.preventDefault();
+                            fnDraw($('a', this).text());
+                        });
+            }
+
+            if (settings.iCurrentPage === 1) {
+                $('.first', base).addClass('disabled');
+            } else {
+                $('.first', base).removeClass('disabled');
+            }
+
+            if (settings.iCurrentPage === settings.iTotalPages || settings.iTotalPages === 0) {
+                $('.last', base).addClass('disabled');
+            } else {
+                $('.last', base).removeClass('disabled');
+            }
+
+        };
+
+        return this.each(function() {
+
+            if (Object.keys(cache.json).length === 0) {
+                fnGetData();
+            }
+
+            $(base).addClass('pagination  pagination-right').append(
+                    '<ul class="pagination">' +
+                    '<li class="first"><a href="#">&laquo;</a></li>' +
+                    '<li class="last"><a href="#">&raquo;</a></li>' +
+                    '</ul>'
+                    );
+            var els = $('a', this);
+            $(els[0]).bind('click', function(e) {
+                e.preventDefault();
+                fnDraw(1);
+            });
+            $(els[1]).bind('click', function(e) {
+                e.preventDefault();
+                fnDraw(settings.iTotalPages);
+            });
+            fnRenderPaging();
+            fnSentData();
+        });
+    };
+})(jQuery);
